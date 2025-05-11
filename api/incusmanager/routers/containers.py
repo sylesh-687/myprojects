e035f2dfe5b611d800b222b4c6fec15d6dcd20ec
kali <kali@kali.bhairahawa>
2025-05-11 01:09:44 -0700

    Feat: Added Methods for List Networks, List Profiles and Container Create

--- api/incusmanager/routers/containers.py
+++ api/incusmanager/routers/containers.py
@@ -4,20 +4,26 @@ import json
 
 router=APIRouter()
 
-@router.get("/list")
+@router.get("/list/containers")
 async def list_containers():
     process = subprocess.run(['incus', 'list', '-f', 'json'], capture_output=True, text=True)
     containers = json.loads(process.stdout)
-    output = []
-    for c in containers:
-        name = c.get("name")
-        status = c.get("status")
-        ips = c.get("state", {}).get("network", {}).get("eth0", {}).get("addresses", [])
-        ip = next((a["address"] for a in ips if a["family"] == "inet"), None)
+    return containers
 
-        output.append({
-            "name": name,
-            "ip": ip,
-            "status": status
-        })
-    return output
+@router.get("/list/networks")
+async def list_networks():
+    process=subprocess.run(['incus','network','list','-f','json'],capture_output=True,text=True)
+    networks=json.loads(process.stdout)
+    return networks
+
+@router.get("/list/profiles")
+async def list_profiles():
+    process=subprocess.run(['incus','profile','list','-f','json'],capture_output=True,text=True)
+    profiles=json.loads(process.stdout)
+    return profiles
+
+@router.post("/create/instance")
+async def create_instance():
+    process=subprocess.run(['incus','launch','images:debian/12','NewInstance'],capture_output=True,text=True)
+    instance=json.loads(process.stdout)
+    return instance
