a2c1977a39651d728c293599bbaa175ea360f698
kali <kali@kali.bhairahawa>
2025-06-09 04:56:40 -0700

    Added few more apis'

--- api/incusmanager/main.py
+++ api/incusmanager/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from routers import containers
+from routers import networks
 
 app = FastAPI(title="IncusManager API", version="1.0.0")
 app.include_router(containers.router)
+app.include_router(networks.router)
--- api/incusmanager/routers/containers.py
+++ api/incusmanager/routers/containers.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-import subprocess
 import requests_unixsocket
 
 
@@ -9,12 +8,22 @@ BASE_URL=f"http+unix://{ENCODED_SOCKET_PATH}"
 
 router=APIRouter()
 
-@router.get("/list")
+@router.get("/list/containers")
 async def list_containers():
     session=requests_unixsocket.Session()
     instances_url=f"{BASE_URL}/1.0/instances"
     response = session.get(instances_url)
     return response.json()
+
+
+@router.get("/list/containers/details")
+async def list_containers_detail():
+    session=requests_unixsocket.Session()
+    instances_url=f"{BASE_URL}/1.0/instances?recursion=1"
+    response = session.get(instances_url)
+    return response.json()
+
+
 
 
 
